737ab248def4bc7144c0b6b8b738d65dfcb6d8db
ophub <ophub2020@gmail.com>
2022-04-28 19:13:48 +0800

    Adjust the code format

--- luci-app-amlogic/root/usr/lib/lua/luci/fs.lua
+++ luci-app-amlogic/root/usr/lib/lua/luci/fs.lua
@@ -22,14 +22,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-]]--
+]] --
 
 local io    = require "io"
 local os    = require "os"
 local ltn12 = require "luci.ltn12"
-local fs	= require "nixio.fs"
+local fs    = require "nixio.fs"
 local nutil = require "nixio.util"
-
 local type  = type
 
 --- LuCI filesystem library.
@@ -149,8 +148,8 @@ function dir(...)
 	local iter, code, msg = fs.dir(...)
 	if iter then
 		local t = nutil.consume(iter)
-		t[#t+1] = "."
-		t[#t+1] = ".."
+		t[#t + 1] = "."
+		t[#t + 1] = ".."
 		return t
 	else
 		return nil, code, msg
